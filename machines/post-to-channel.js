9a4275c04c3b553be8948ba3b2fbc2250ac4b50f
Scott Gress <scottmgress@gmail.com>
2015-03-14 15:24:54 -0700

    Update to new version of Slack API

--- machines/post-to-channel.js
+++ machines/post-to-channel.js
@@ -3,36 +3,37 @@ module.exports = {
   description: 'Post a message to the specified channel in Slack.',
   extendedDescription: 'Unless otherwise configured, this machine posts to the "#general" channel.',
   inputs: {
-    subdomain: {
-      description: 'A "Slack domain"',
-      example: 'my-company-name',
-      required: true,
-      whereToGet: {
-        url: 'https://slack.com/signin',
-        description: 'Copy a "Slack domain" (i.e. the subdomain prefix you use when logging in to Slack)'
-      }
-    },
-    webhookToken: {
-      description: 'A Slack webhook token',
-      example: 'XLwUYCLQsA4PXysVkO7cWWQp',
+    webhookUrl: {
+      description: 'A Slack webhook URL',
+      example: 'https://hooks.slack.com/services/xxxxyyyy/ZZZZZZZZ/XXXXXXXXXXXXXXX',
       required: true,
       whereToGet: {
         url: 'https://slack.com/services/new/incoming-webhook',
-        description: 'Setup a new webhook, then copy everything after the final slash ("/") in the "Webhook URL".',
+        description: 'Setup a new webhook, then copy the "Webhook URL".',
         extendedDescription: 'You will need to be logged in to Slack, or sign up for an account if you haven\'t already.'
-      }
+      },
+      friendlyName: "Webook URL"
     },
     username: {
       description: 'The username to use when posting',
-      example: 'someboty'
+      example: 'someboty',
+      friendlyName: "Username"
     },
     channel: {
       description: 'The Slack channel where the post should be sent',
-      example: '#general'
+      example: '#general',
+      friendlyName: "Channel"
     },
     message: {
       description: 'The message to post',
-      example: 'Good morning, boys and girls!'
+      example: 'Good morning, boys and girls!',
+      friendlyName: "Message"
+    },
+    iconEmoji: {
+      example: ':ghost:',
+      friendlyName: 'Icon Emoji',
+      description: "The bot icon to display next to the post.",
+      whereToGet: "http://www.emoji-cheat-sheet.com/"
     }
   },
   defaultExit: 'success',
@@ -50,11 +51,13 @@ module.exports = {
   fn: function(inputs, exits) {
     var Slack = require('slack-node');
 
-    var slack = new Slack(inputs.webhookToken, inputs.subdomain);
+    var slack = new Slack();
+    slack.setWebhook(inputs.webhookUrl);
 
     slack.webhook({
       channel: inputs.channel || "#general",
       username: inputs.username || 'machinepack-slack',
+      icon_emoji: inputs.iconEmoji,
       text: inputs.message||'Hello world!  Posted from the `post-to-channel` machine in `machinepack-slack`.'
     }, function(err, response) {
       if (err) return exits.error(err);
